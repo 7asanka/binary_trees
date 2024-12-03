e5bf526b3b4c06ccda522a958bd7992abf2caaad
7asanka <hassan2002_kimo@hotmail.com>
2024-12-03 09:36:16 -0800

    0x1C task 3 fix; ;

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -12,12 +12,7 @@ void binary_tree_delete(binary_tree_t *tree)
 	if (tree == NULL)
 		return;
 
-	binary_tree_t *left = tree->left;
-	binary_tree_t *right = tree->right;
-
-	if (tree->left != NULL)
-		binary_tree_delete(left);
-	if (tree->right != NULL)
-		binary_tree_delete(right);
+	binary_tree_delete(left);
+	binary_tree_delete(right);
 	free(tree);
 }
